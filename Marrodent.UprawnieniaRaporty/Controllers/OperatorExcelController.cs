dc22d1f0a4aa4c7fd8bc6d9962db8093f18739f0
dawid.grzeszuk <dawid.grzeszuk@marrodent.pl>
2022-12-12 12:18:03 +0100

    OperatorExcelController

--- Marrodent.UprawnieniaRaporty/Controllers/OperatorExcelController.cs
+++ Marrodent.UprawnieniaRaporty/Controllers/OperatorExcelController.cs
@@ -25,10 +25,16 @@ namespace Marrodent.UprawnieniaRaporty.Controllers
             {
                 using (ExcelPackage package = new ExcelPackage(stream))
                 {
-                    ExcelWorksheet excelWorksheet = package.Workbook.Worksheets.Add("Uprawnienia");
-                    PrepareColumns(ref excelWorksheet);
-                    AddData(ref excelWorksheet, _data);
-                    AutoFit(ref excelWorksheet);
+                    ExcelWorksheet excelWorksheetPermissions = package.Workbook.Worksheets.Add("Uprawnienia");
+                    PrepareColumnsPermissions(ref excelWorksheetPermissions);
+                    AddDataPermissions(ref excelWorksheetPermissions, _data);
+                    AutoFit(ref excelWorksheetPermissions);
+
+                    ExcelWorksheet excelWorksheetLastLogin = package.Workbook.Worksheets.Add("Ostatnie logowanie");
+                    PrepareColumnsLastLogin(ref excelWorksheetLastLogin);
+                    AddDataLastLogin(ref excelWorksheetLastLogin, _data);
+                    AutoFit(ref excelWorksheetLastLogin);
+
                     package.Save();
                     package.Dispose();
                 }
@@ -39,7 +45,7 @@ namespace Marrodent.UprawnieniaRaporty.Controllers
         }
 
         //Private
-        private void PrepareColumns(ref ExcelWorksheet worksheet)
+        private void PrepareColumnsPermissions(ref ExcelWorksheet worksheet)
         {
             worksheet.Cells[1, 1].Value = "Kod operatora";
             worksheet.Cells[1, 2].Value = "Nazwa operatora";
@@ -49,8 +55,14 @@ namespace Marrodent.UprawnieniaRaporty.Controllers
                 worksheet.Cells[1, x].Value = $"Uprawnienie {x - 2}";
             }
         }
+        private void PrepareColumnsLastLogin(ref ExcelWorksheet worksheet)
+        {
+            worksheet.Cells[1, 1].Value = "Kod operatora";
+            worksheet.Cells[1, 2].Value = "Nazwa operatora";
+            worksheet.Cells[1, 3].Value = "Ostatnie logowanie";
+        }
 
-        private void AddData(ref ExcelWorksheet worksheet, ICollection<OperatorExcelRow> data)
+        private void AddDataPermissions(ref ExcelWorksheet worksheet, ICollection<OperatorExcelRow> data)
         {
             int row = 2;
 
@@ -63,6 +75,20 @@ namespace Marrodent.UprawnieniaRaporty.Controllers
                 {
                     worksheet.Cells[row, x].Value = line.Permissions[x - 3];
                 }
+
+                row++;
+            }
+        }
+
+        private void AddDataLastLogin(ref ExcelWorksheet worksheet, ICollection<OperatorExcelRow> data)
+        {
+            int row = 2;
+
+            foreach (var line in data)
+            {
+                worksheet.Cells[row, 1].Value = line.Name;
+                worksheet.Cells[row, 2].Value = line.FullName;
+                worksheet.Cells[row, 3].Value = line.LastLogin;
 
                 row++;
             }
--- Marrodent.UprawnieniaRaporty/Models/OperatorExcelRow.cs
+++ Marrodent.UprawnieniaRaporty/Models/OperatorExcelRow.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Marrodent.UprawnieniaRaporty.Models
 {
     public sealed class OperatorExcelRow
     {
+        //Public - string
         public string Name { get; set; }
         public string FullName { get; set; }
+
+        //Public - datetime
+        public DateTime LastLogin { get; set; }
+
+        //Public - list
         public List<string> Permissions { get; set; }
     }
 }
